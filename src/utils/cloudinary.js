113f5b5b6681a32553d5646b3430970bd4e5967c
geekysky <debu69980@gmail.com>
2025-10-22 00:32:24 +0530

    added cloudinary upload and multer middleware to upload file from local express server to cloudinary/s3 and to upload file to local express server from user end

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,  
+  api_secret: ProcessingInstruction.env.CLOUDINARY_API_SECRET
+});
+
+const uploadonCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath){
+            return null;
+        }
+        
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        });
+
+        //file has been uploaded successfully on cloudinary
+        console.log("File successfuly uploaded on cloudinary!",response.url);
+
+        return response;
+
+
+    } catch (error) {
+        //file local server p to aa chuka hai but cloudinary for any reason upload nahi ho paya hai...
+        
+        //remove the locally saved temporary file as the upload operation failed 
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+};
+
+// cloudinary.uploader
+//   .upload("my_image.jpg")
+//   .then(result=>console.log(result));
+
+
+export {uploadonCloudinary};
